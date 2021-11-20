98905b350b94c32e53155e796092ce0157c6d941
Sachille Atapattu <sa2257@cornell.edu>
2021-11-20 09:53:30 -0500

    Add crontab to schedule, but does not work on Mac

--- oasis_schedule.py
+++ oasis_schedule.py
@@ -1,1 +1,40 @@
 # Read user directives and app names txt files and generate a crontab file
+
+# Importing the CronTab class from the module
+from crontab import CronTab
+
+# Creating an object from the class
+# Using the root user
+cron = CronTab(user="sachille")
+
+# Using the current user
+#my_cron = CronTab(user=True)
+
+# Creating an object from the class into a file
+#file_cron = CronTab(tabfile="filename.tab")
+
+# Creating a new job
+#job = cron.new(command='echo hello_world')
+
+# Setting up restrictions for the job
+# The job takes place once every 5 minutes
+# job.minute.every(5)
+
+# The job takes place once every four hours
+# job.hour.every(4)
+
+# The job takes place on the 4th, 5th, and 6th day of the week.
+#job.day.on(4, 5, 6)
+
+# Clearing the restrictions of a job
+# job.clear()
+
+# cron.write()
+
+job = cron.new(
+    command='echo hello_world > /Users/Shared/workspace/zerzura/oasis/sysout.log')
+# job2 = cron.new(
+#    command='pwd >> /Users/Shared/workspace/zerzura/oasis/sysout.log')
+job.minute.every(1)
+# job2.minute.every(1)
+cron.write()
