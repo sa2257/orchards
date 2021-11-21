da992c99567eb730fdae31d8b361f423ff35f1a8
Sachille Atapattu <sa2257@cornell.edu>
2021-11-21 18:48:28 -0500

    Add monitoring functions

--- sentinel_monitor.py
+++ sentinel_monitor.py
@@ -1,1 +1,13 @@
 # Monitor sensor variation, battery level and user directives txt and generate energy policy
+
+
+def check_liveness(sensor):
+    # We hope the sensor is live
+    live = True
+    return live
+
+
+def check_diffness(sensor):
+    # We hope the sensor values are different
+    diff = True
+    return diff
