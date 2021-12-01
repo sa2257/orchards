bed684b9e1e82766b4ebd00e1ef47515346f0064
Sachille Atapattu <sa2257@cornell.edu>
2021-12-01 15:30:08 -0500

    Add gps read

--- gps_sensor.py
+++ gps_sensor.py
@@ -8,6 +8,23 @@ DIFF_GPS = [42.444125, -76.462797]
 # https://learn.adafruit.com/adafruit-ultimate-gps/circuitpython-parsing
 
 
+def ultimate_uart():
+    import serial
+    import pynmea2
+    while True:
+        port = "/dev/ttyAMAO"
+        ser = serial.Serial(port, baudrate=9600, timeout=0.5)
+        dataout = pynmea2.NMEAStreamReader()
+        newdata = ser.readline()
+        if newdata[0:6] == "$GPRMC":
+            newmsg = pynmea2.parse(newdata)
+            lat = newmsg.latitude
+            lng = newmsg.longitude
+            gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
+            print(gps)
+            return [lat, lng]
+
+
 def gps_read():
     return GPS
 
