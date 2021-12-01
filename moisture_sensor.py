a0756ab08e280c54cab0b4356baff7d012616fd0
Sachille Atapattu <sa2257@cornell.edu>
2021-12-01 12:32:06 -0500

    100 sampling approach test

--- moisture_sensor.py
+++ moisture_sensor.py
@@ -22,6 +22,14 @@ def grove_read():
         return sensor.moisture
 
 
+def grove_read_mean(times):
+    sum = 0
+    for i in range(times):
+        sum += grove_read()
+        time.sleep(0.1)
+    return sum/times
+
+
 if __name__ == '__main__':
     if EXAGGERATE:
         while True:
@@ -29,7 +37,7 @@ if __name__ == '__main__':
             moistdata_string = 'Moisture value: {0} (-)kPa'.format(
                 sensor_value)
             print(moistdata_string)
-            time.sleep(0.01)
+            time.sleep(55)
     else:
         sensor_value = grove_read()
         #moistdata_string = 'Moisture value: {0} (-)kPa'.format(sensor_value)
