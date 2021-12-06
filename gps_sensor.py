0cf9cdc87a31f3e50f67639f61f9f7e27e942a5c
Sachille Atapattu <sa2257@cornell.edu>
2021-12-06 15:36:50 -0500

    Read GPS

--- gps_sensor.py
+++ gps_sensor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys
 from orchards_time import time_now
 import time
@@ -9,24 +11,26 @@ DIFF_GPS = [42.444125, -76.462797]
 
 
 def ultimate_uart():
+    
     import serial
     import pynmea2
-    while True:
-        port = "/dev/ttyAMAO"
-        ser = serial.Serial(port, baudrate=9600, timeout=0.5)
-        dataout = pynmea2.NMEAStreamReader()
-        newdata = ser.readline()
-        if newdata[0:6] == "$GPRMC":
-            newmsg = pynmea2.parse(newdata)
-            lat = newmsg.latitude
-            lng = newmsg.longitude
-            gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
-            print(gps)
-            return [lat, lng]
+    #while True:
+    port = "/dev/ttyAMA0"
+    ser = serial.Serial(port, baudrate=9600, timeout=0.5)
+    dataout = pynmea2.NMEAStreamReader()
+    newdata = ser.readline()
+    if newdata[0:6] == "$GPRMC":
+        newmsg = pynmea2.parse(newdata)
+        lat = newmsg.latitude
+        lng = newmsg.longitude
+        gps = "Latitude = " + str(lat) + " and Longitude = " + str(lng)
+        print(gps)
+        return [lat, lng]
 
 
 def gps_read():
     return GPS
+    #return ultimate_uart()
 
 
 def diff_read():
