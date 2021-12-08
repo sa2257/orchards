83e79326f992611d681e601a7e3ffd3e4cc77527
Sachille Atapattu <sa2257@cornell.edu>
2021-12-08 17:02:44 -0500

    Add optee client commands

--- optee_config.py
+++ optee_config.py
@@ -1,44 +1,40 @@
 import os
 import sys
-from crontab import CronTab
+# from crontab import CronTab
 
-# Using the current user
-cron = CronTab(user=True)
+# Using the current user cron = CronTab(user=True)
 
+# def create_command(): script_name = 'optee_config.py' command = '{} {}/{} {}
+#     {}'.format('python', os.getcwd(), script_name, '--option', '1')
 
-def create_command():
-    script_name = 'optee_config.py'
-    command = '{} {}/{} {} {}'.format('python', os.getcwd(),
-                                      script_name, '--option', '1')
+#     return command
 
-    return command
+# def schedule_make(): command = create_command()
 
+#     job = cron.new(command=command) job.every_reboot()
 
-def schedule_make():
-    command = create_command()
-
-    job = cron.new(command=command)
-    job.every_reboot()
-
-    # clean and write to cron table
-    cron.write()
+#     # clean and write to cron table
+#     cron.write()
 
 
 def run_make():
     # run cmake commands from optee_client
-    pass
+    os.chdir('/home/pi/optee_client/build')
+    os.system(
+        'cmake -DCMAKE_C_COMPILER=arm-linux-gnueabihf-gcc -DCMAKE_INSTALL_PREFIX=/tmp/optee_client ..')
+    os.system('make')
+    os.system('make install')
+    os.chdir('/home/pi')
+    os.system('sudo /tmp/optee_client/sbin/tee-supplicant -d')
 
 
 if __name__ == '__main__':
-    import argparse
+    # import argparse
 
-    parser = argparse.ArgumentParser(description='Initial settings for OPTEE')
-    parser.add_argument('--option', metavar='string', required=True,
-                        help='options are 0 and 1')
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description='Initial settings for OPTEE')
+    # parser.add_argument('--option', metavar='string', required=True,
+    # help='options are 0 and 1') args = parser.parse_args()
 
-    if(args.option == '0'):
-        schedule_make()
-    else:  # '1'
-        run_make()
+    # if(args.option == '0'): schedule_make() else:  # '1'
+    run_make()
     sys.exit()
